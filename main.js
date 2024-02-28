77c68fc1416d18289ce5e6696ff9996aacd397a3
gustavossanches <gustavo.sanches80@hotmail.com>
2024-02-27 21:06:25 -0300

    ajustes na criação do jogo

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 
 
 criarJogo('teste')
-
 function criarJogo(idElemento){
-
 const mainContainer = document.getElementById(idElemento);
 const containerJogo = document.createElement("div")
 
@@ -12,25 +10,25 @@ const divColuna1 = document.createElement("div")
 const dado11 = document.createElement("div")
 dado11.innerHTML = '1'
 const dado12 = document.createElement("div")
-dado12.innerHTML = '2'
+dado12.innerHTML = '4'
 const dado13 = document.createElement("div")
-dado13.innerHTML = '3'
+dado13.innerHTML = '7'
 
 //criar a Coluna 2 e seus dados
 const divColuna2 = document.createElement("div")
 const dado21 = document.createElement("div")
-dado21.innerHTML = '4'
+dado21.innerHTML = '2'
 const dado22 = document.createElement("div")
 dado22.innerHTML = '5'
 const dado23 = document.createElement("div")
-dado23.innerHTML = '6'
+dado23.innerHTML = '8'
 
 //criar a Coluna 3 e seus dados
 const divColuna3 = document.createElement("div")
 const dado31 = document.createElement("div")
-dado31.innerHTML = '7'
+dado31.innerHTML = '3'
 const dado32 = document.createElement("div")
-dado32.innerHTML = '8'
+dado32.innerHTML = '6'
 const dado33 = document.createElement("div")
 dado33.innerHTML = '9'
 
@@ -61,4 +59,10 @@ containerJogo.children[2].classList.add('coluna-3')
 
 //usar o parametro da função
 mainContainer.appendChild(containerJogo).classList.add('containerJogo')
+}
+
+const botoes = document.getElementsByClassName('dado')
+
+for(const i of botoes){
+    
 }
